7f05e348c1ab15f8bbbfe5b4c13cfa9420bea1f3
knixan <josefineeriksson@live.se>
2025-08-14 21:34:44 +0200

    favicon

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -35,7 +35,7 @@ export default function Head() {
         rel="stylesheet"
         href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"
       />
-      <link rel="icon" href="/favicon.png" type="image/png" />
+      <link rel="icon" href="/public/favicon.png" type="favicon/png" />
     </>
   );
 }
