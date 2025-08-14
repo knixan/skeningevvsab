d154455a8c153c397c169ac74c037b75ec2fc275
knixan <josefineeriksson@live.se>
2025-08-14 21:30:35 +0200

    favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,13 +2,6 @@ import "./globals.css";
 import { Roboto } from "next/font/google";
 import React from "react";
 
-export const metadata = {
-  title: "Min app",
-  icons: {
-    icon: "/favicon.png",
-  },
-};
-
 const roboto = Roboto({
   subsets: ["latin"],
   weight: ["400", "500", "700"],
